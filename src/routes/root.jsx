c927ad2e189d5193e3ebba09b0538fefb95d4887
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2023-05-24 12:54:56 +0700

    Step 11 : Global Pending UI

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, NavLink, Link, useLoaderData, Form, redirect } from "react-router-dom";
+import { Outlet, useNavigation, NavLink, Link, useLoaderData, Form, redirect } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -13,6 +13,7 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
     return (
       <>
         <div id="sidebar">
@@ -74,7 +75,10 @@ export default function Root() {
           )}
           </nav>
         </div>
-        <div id="detail">
+        <div id="detail"
+          className={
+            navigation.state === "loading" ? "loading" : ""
+          }>
           <Outlet />
         </div>
       </>
