a33d8c09b85d3f519d91257b79fc53f23fbd08c2
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2023-05-24 15:47:13 +0700

    Step 12 : Deleting Records

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,8 @@ import Contact, {
 import EditContact, {
   action as editAction,
 } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+
 
 const router = createBrowserRouter([
   {
@@ -32,6 +34,11 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
